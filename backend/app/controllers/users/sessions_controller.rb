a98ab8386a584d11a1fdebea925e24fa93f5356d
Ricky Mormor <rickymormor1@gmail.com>
2022-11-26 23:56:41 +0000

    ✅ FEAT: allow sessions controller to respond to json

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+
+  respond_to :json
+
+  private
+
+  def respond_with(resource, options = {})
+    if resource.persisted?
+      render json: { status: { code: 200, message: 'Signed in successfully.', data: resource } }
+    else
+      render json: { status: { message: 'Sign in failed.', errors: resource.errors.full_messages },
+      code: :unprocessable_entity }
+    end
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
