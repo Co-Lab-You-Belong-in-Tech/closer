ac48c5b271e9d811771202ae3a9437f7f02bf430
Ricky Mormor <rickymormor1@gmail.com>
2022-11-26 20:54:30 +0000

    ✅ FEAT: setup devise jwt

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -308,4 +308,14 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.dispatch_requests = [
+      ['POST', %r{^/api/v1/auth/sign_in$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/api/v1/auth/sign_out$}]
+    ]
+    jwt.expiration_time = 1.day.to_i
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, controllers: {
+    sessions: 'api/v1/auth/sessions',
+    registrations: 'api/v1/auth/registrations'
+  }
 end
