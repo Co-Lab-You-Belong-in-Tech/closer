5df38ddb674c5b2d396c5258a18b8c95294a1fad
Ricky Mormor <rickymormor1@gmail.com>
2022-12-07 12:02:10 +0000

    ✅ FEAT: create auth queries

--- src/features/api/auth.ts
+++ src/features/api/auth.ts
@@ -11,3 +11,25 @@ export const registerUser = async (formData: object) => {
         });
     return data;
 }
+
+export const loginUser = async (formData: object) => {
+    const {data} = await axios({
+        method: 'POST',
+        url: `${API_URL}/users/sign_in`,
+        data: {
+            user : formData
+        }
+        });
+    return data;
+}
+
+export const logOutUser = async () => {
+    const {data} = await axios({
+        method: 'DELETE',
+        url: `${API_URL}/users/sign_out`,
+        headers: {
+            Authorization: `Token ${localStorage.getItem('token')}`
+        }
+        });
+    return data;
+}
