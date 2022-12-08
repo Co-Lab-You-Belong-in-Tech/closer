37dfd6ee30f488829019e796dc53510281c2ccbe
Ricky Mormor <rickymormor1@gmail.com>
2022-12-08 08:01:36 +0000

    ✅ FEAT: fix userStatus store and add triggers store

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,41 +1,52 @@
 import create from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
-const userStatusStore = (set: (arg0: { userStatus: string }) => any) => ({
-  userStatus: "",
-  setUserStatus: (status: string) => set({ userStatus: status }),
-});
+interface DefaultProps{
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
-// const triggerStore = (
-//   set: (arg0: {
-//     (state: any): { triggers: any[] };
-//     (state: any): { triggers: any };
-//   }) => any
-// ) => ({
-//   triggers: [],
-//   addTrigger: (trigger: any) =>
-//     set((state: { triggers: any }) => ({
-//       triggers: [...state.triggers, trigger],
-//     })),
-//   removeTrigger: (trigger: any) =>
-//     set((state: { triggers: any[] }) => ({
-//       triggers: state.triggers.filter((t: any) => t !== trigger),
-//     })),
-// });
 
 export const useUserStatusStore = create(
   persist(
-    devtools(userStatusStore, {
-      name: "userStatus",
-    })
+    devtools(
+      (set: (arg0: { userStatus: string }) => any) => ({
+        userStatus: "",
+        setUserStatus: (status: string) => set({ userStatus: status }),
+      }),
+      {
+        name: "userStatus",
+      }
+    )
   )
 );
 
-// export const useTriggersStore = create(
-//   persist(
-//     devtools(triggerStore, {
-//       name: "triggers",
-//     }
-//   ),
-//   )
-// );
+// This code creates a store for the triggers component.
+// The store contains an array of triggers and functions to add and remove triggers.
+const TriggersStore = create<{
+  triggers: DefaultProps[];
+  addTrigger: (trigger: DefaultProps) => void;
+  removeTrigger: (trigger: DefaultProps) => void;
+}>( (set) => ({
+  triggers: [],
+  addTrigger: (trigger) =>
+    set((state) => ({
+      triggers: [...state.triggers, trigger],
+    })),
+  removeTrigger: (trigger) =>
+    set((state) => ({
+      triggers: state.triggers.filter((t) => t !== trigger),
+    })),
+}));
+
+
+// Creates a store and persists it to local storage
+export const useTriggersStore = create(
+  persist(
+    devtools( TriggersStore, {
+      name: "triggers",
+    })
+  )
+);
