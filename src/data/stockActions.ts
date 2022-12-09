3b45ae0301a4043f2ec06700a81720a2999280a1
Ricky Mormor <rickymormor1@gmail.com>
2022-12-09 18:20:53 +0000

    ✅ FEAT: create stocks to replace repeated vreated buttons

---
+++ src/data/stockActions.ts
@@ -0,0 +1,15 @@
+// Array that holds all the stock actions
+export const stockActions  = [
+  "attacked",
+  "complained",
+  "expressed frustration",
+  "yelled",
+  "made threats",
+  "criticized",
+  "zone out",
+  "analyzed",
+  "became quiet",
+  "walked away",
+  "sulked",
+  "tried to reason"
+];
