b261b0fcf626cc38bfd1b8dce092cf6b52597282
Jingru Xu <jingruxu@Jingrus-Air.lan>
2022-12-14 22:17:57 -0500

    cta button update

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -1,37 +1,50 @@
+import React, { useState } from "react";
+
 import {
   IonButtons,
   IonBackButton,
   IonIcon,
   IonButton,
-  IonRouterLink,
-  useIonToast,
+  useIonAlert,
 } from "@ionic/react";
 import { close } from "ionicons/icons";
+import { Redirect } from "react-router-dom";
 
 const Cta: React.FC = () => {
-  const [present] = useIonToast();
-  const presentToast = (position: "top" | "middle" | "bottom") => {
-    present({
-      message: "Backing to Dashboard",
-      duration: 600,
-      position: position,
-    });
-  };
+  const [presentAlert] = useIonAlert();
+  const [handlerMessage, setHandlerMessage] = useState(false);
+
   return (
     <>
       <IonButtons>
         <IonBackButton />
       </IonButtons>
-      <IonRouterLink routerLink="dashboard">
-        <IonButton
-          className="cta"
-          fill="clear"
-          size="small"
-          onClick={() => presentToast("top")}
-        >
-          <IonIcon icon={close} color="dark"></IonIcon>
-        </IonButton>
-      </IonRouterLink>
+      <IonButton
+        className="cta"
+        fill="clear"
+        size="small"
+        onClick={() =>
+          presentAlert({
+            header:
+              "Are you sure to return to the dashboard? Exiting now will lose your progress.",
+
+            buttons: [
+              {
+                text: "No",
+              },
+              {
+                text: "Yes",
+                handler: () => {
+                  setHandlerMessage(true);
+                },
+              },
+            ],
+          })
+        }
+      >
+        <IonIcon icon={close} color="dark"></IonIcon>
+      </IonButton>
+      {handlerMessage === true && <Redirect to="dashboard" />}
     </>
   );
 };
