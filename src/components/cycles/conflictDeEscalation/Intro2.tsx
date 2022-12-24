900b925920d9395210a53034b494cd656092d6c7
Ricky Mormor <rickymormor1@gmail.com>
2022-12-24 17:57:31 +0000

    👌 IMPROVE: formate page

--- src/components/cycles/conflictDeEscalation/Intro2.tsx
+++ src/components/cycles/conflictDeEscalation/Intro2.tsx
@@ -1,11 +1,4 @@
-import {
-  IonContent,
-  IonButton,
-  IonText,
-} from "@ionic/react";
-
-import Cta from "../../Cta";
-import IntroImage from "../assets/IntroImage.png";
+import { IonContent, IonButton, IonText } from "@ionic/react";
 
 interface FuncProps {
   handleProgress?: () => void;
@@ -13,33 +6,37 @@ interface FuncProps {
 
 const Intro2: React.FC<FuncProps> = (handleProgress) => {
   return (
+    <IonContent>
+      <div className="introText">
+        <IonText color="light">
+          <p style={{ width: "80%", margin: "0 auto" }}>
+            Exercise time:10mins
+            <br></br>
+            <br></br>
+            Great job taking the first step! In a few moment, we will walk
+            through what EFT calls the conflict de-escalation cycle.
+            <br></br>
+            <br></br>
+            You will need to reflect on your triggers, feelings, and actions in
+            this specific conflict you have in mind. Reflecting on your emotions
+            can be overwhelming. If at any moment you feel too overwhelmed to
+            continue, take a break, BREAAAATHE, and come back when you're
+            ready."
+          </p>
+        </IonText>
+      </div>
 
-      <IonContent>
-        <div className="introText">
-          <IonText color="light">
-            <p style={{ width: "80%", margin: "0 auto" }}>
-              Exercise time:10mins
-              <br></br>
-              <br></br>
-              Great job taking the first step! In a few moment, we will walk
-              through what EFT calls the conflict de-escalation cycle.
-              <br></br>
-              <br></br>
-              You will need to reflect on your triggers, feelings, and actions
-              in this specific conflict you have in mind. Reflecting on your
-              emotions can be overwhelming. If at any moment you feel too
-              overwhelmed to continue, take a break, BREAAAATHE, and come back
-              when you're ready."
-            </p>
-          </IonText>
-        </div>
-
-          <div className="ion-padding">
-            <IonButton onClick={() => handleProgress.handleProgress!()} color="primary" className="ion-text-center buttonStyle">
-              Let's do this!
-            </IonButton>
-          </div>
-      </IonContent>
+      <div className="ion-padding">
+        {/* TODO: Change how handleProgress function is called */}
+        <IonButton
+          onClick={() => handleProgress.handleProgress!()}
+          color="primary"
+          className="ion-text-center buttonStyle"
+        >
+          Let's do this!
+        </IonButton>
+      </div>
+    </IonContent>
   );
 };
 
