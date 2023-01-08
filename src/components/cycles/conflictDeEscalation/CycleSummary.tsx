fd916a6def1c0a9a119681ecbb547059e4096ec4
Jingru Xu <jingruxu@Jingrus-Air.lan>
2023-01-08 17:54:41 -0500

    minor styling fix

--- src/components/cycles/conflictDeEscalation/CycleSummary.tsx
+++ src/components/cycles/conflictDeEscalation/CycleSummary.tsx
@@ -27,15 +27,14 @@ const CycleSummary: React.FC<FuncProps> = (props) => {
               When{" "}
               <span className="cycleSummarySpan">
                 {" "}
-                dirty dishes in the sink{" "}
-              </span>
-              happens, I feel
-              <span className="cycleSummarySpan">annoyed</span> , and I
-              <span className="cycleSummarySpan">criticized you</span>. The more
-              I<span className="cycleSummarySpan">pursue</span>, the more you{" "}
-              <span className="cycleSummarySpan">sulk</span>. But deep down, I
-              feel <span className="cycleSummarySpan">insecure</span>and fear
-              that{" "}
+                dirty dishes in the sink
+              </span>{" "}
+              happens, I feel <span className="cycleSummarySpan">annoyed</span>,
+              and I <span className="cycleSummarySpan"> criticized you</span>.
+              The more I <span className="cycleSummarySpan">pursue</span>, the
+              more you <span className="cycleSummarySpan">sulk</span>. But deep
+              down, I feel <span className="cycleSummarySpan">insecure</span>{" "}
+              and fear that{" "}
               <span className="cycleSummarySpan">
                 {" "}
                 I'm not good enough for you
