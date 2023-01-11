f252ee8439580f478ec119c3effd040a413a0a73
Jingru Xu <jingruxu@Jingrus-Air.lan>
2023-01-11 18:15:10 -0500

    tried to add stuff on summary page

--- src/components/cycles/conflictDeEscalation/CycleSummary.tsx
+++ src/components/cycles/conflictDeEscalation/CycleSummary.tsx
@@ -1,3 +1,4 @@
+import { useStore } from "../../../features/store";
 import {
   IonButton,
   IonCard,
@@ -12,6 +13,10 @@ interface FuncProps {
 }
 
 const CycleSummary: React.FC<FuncProps> = (props) => {
+  const myactionDisplay = useStore((state) => state.actions);
+  const partnerAction = useStore((state) => state.partnerActions);
+  const pain1 = useStore((state) => state.pain1);
+  const pain2 = useStore((state) => state.pain2);
   return (
     <>
       <IonHeader>
@@ -20,17 +25,14 @@ const CycleSummary: React.FC<FuncProps> = (props) => {
         <IonCard style={{ background: "#F8F8F8" }}>
           <IonCardHeader>
             <IonCardContent>
-              When{" "}
-              <span className="cycleSummarySpan">
-                {" "}
-                dirty dishes in the sink
-              </span>{" "}
-              happens, I feel <span className="cycleSummarySpan">annoyed</span>,
-              and I <span className="cycleSummarySpan"> criticized you</span>.
-              The more I <span className="cycleSummarySpan">pursue</span>, the
-              more you <span className="cycleSummarySpan">sulk</span>. But deep
-              down, I feel <span className="cycleSummarySpan">insecure</span>{" "}
-              and fear that{" "}
+              When <span className="cycleSummarySpan"> {myactionDisplay}</span>{" "}
+              happens, I feel <span className="cycleSummarySpan">{pain1}</span>,
+              and I <span className="cycleSummarySpan"> {myactionDisplay}</span>
+              . The more I{" "}
+              <span className="cycleSummarySpan">{myactionDisplay}</span>, the
+              more you <span className="cycleSummarySpan">{partnerAction}</span>
+              . But deep down, I feel {pain2}
+              <span className="cycleSummarySpan">insecure</span> and fear that{" "}
               <span className="cycleSummarySpan">
                 {" "}
                 I'm not good enough for you
